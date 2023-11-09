cfe2277c7468877b34ac3a061e417954e9575b34
swaraj249 <131783662+swaraj249@users.noreply.github.com>
2023-11-09 23:59:49 +0530

    Add files by swaraj

---
+++ Book_Order.java
@@ -0,0 +1,97 @@
+package com.entity;
+
+public class Book_Order {
+	
+	private int id;
+	private String orderId;
+	private String username;
+	private String email;
+	private String phoneno;
+	private String fulladd;
+	private String bookname;
+	private String author;
+	private String price;
+	private String paymentType;
+	public Book_Order() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getBookname() {
+		return bookname;
+	}
+
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneno() {
+		return phoneno;
+	}
+	public void setPhoneno(String phoneno) {
+		this.phoneno = phoneno;
+	}
+	public String getFulladd() {
+		return fulladd;
+	}
+	public void setFulladd(String fulladd) {
+		this.fulladd = fulladd;
+	}
+	public String getPaymentType() {
+		return paymentType;
+	}
+	public void setPaymentType(String paymentType) {
+		this.paymentType = paymentType;
+	}
+	@Override
+	public String toString() {
+		return "Book_Order [id=" + id + ", username=" + username + ", email=" + email + ", phoneno=" + phoneno
+				+ ", fulladd=" + fulladd + ", paymentType=" + paymentType + "]";
+	}
+	
+	
+	
+    
+}
